2ae33e44ed2b499438690c190268c0f881c563db
mifier <2323410354@qq.com>
2020-12-07 22:20:38 +0800

    enter

---
+++ scripts/1.py
@@ -0,0 +1,17 @@
+import  sys
+import numpy as np
+
+
+
+def getone():
+    i=input()
+    print(i)
+
+
+
+
+if __name__ == '__main__':
+    getone()
+
+
+
---
+++ src/1.cpp
@@ -0,0 +1,13 @@
+#include <math.h>
+
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+    printf("Hello World\n");
+    system("pause");
+}
+
+
+ 
