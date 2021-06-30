6ba31463ff6b889e0f8eedc1b503e1574fe3d64c
mifier <2323410354@qq.com>
2021-06-30 16:32:35 +0800

    1

---
+++ java/test.java
@@ -0,0 +1,45 @@
+// import java.awt.*;
+
+// public class test {
+//     public static void main(String[] args) {
+//         Frame f = new Frame("hello");
+//         f.setLayout(null); // 取消布局管理器
+//         f.setSize(900, 600); // 宽300, 高100
+//         Button b = new Button("press me");
+//         b.setSize(100, 30); // 宽100, 高30
+//         b.setLocation(40, 60); // x坐标40, y坐标60
+//         f.add(b);
+//         f.setVisible(true);
+//     }
+// }
+
+//   class Puppy {
+//     int puppyAge;
+
+//     public Puppy(String name) {
+//         // 这个构造器仅有一个参数：name
+//         System.out.println("小狗的名字是 : " + name);
+//     }
+
+//     public void setAge(int age) {
+//         puppyAge = age;
+//     }
+
+//     public int getAge() {
+//         System.out.println("小狗的年龄为 : " + puppyAge);
+//         return puppyAge;
+//     }
+
+//     public static void main(String[] args) {
+//         /* 创建对象 */
+//         //int age;
+//         Puppy myPuppy = new Puppy("tommy");
+//         /* 通过方法来设定age */
+//         myPuppy.setAge(2);
+//         /* 调用另一个方法获取age */
+//       // myPuppy.getAge();
+//         /* 你也可以像下面这样访问成员变量 */
+//         System.out.println("变量值 : " + myPuppy.puppyAge);
+//     }
+// }
+
